131fffda39a6cea32489cde2fb6020f7f639736b
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2022-01-28 13:27:57 -0500

    update slideshows

--- src/components/textImage.js
+++ src/components/textImage.js
@@ -5,19 +5,115 @@ import "slick-carousel/slick/slick-theme.css"
 import { GatsbyImage } from "gatsby-plugin-image"
 import Fade from "react-reveal/Fade"
 
+function SampleNextArrow(props) {
+  const { onClick } = props
+  return (
+    <div
+      className="slideshow-next"
+      onClick={onClick}
+      onKeyDown={onClick}
+      role="button"
+      tabIndex={0}
+      aria-label="go to next"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.617 72.699">
+        <g
+          id="Group_61"
+          data-name="Group 61"
+          transform="translate(-1641.773 -5563.813)"
+        >
+          <line
+            id="Line_93"
+            data-name="Line 93"
+            x2="34.163"
+            y2="36.163"
+            transform="translate(1642.5 5564.5)"
+            fill="none"
+            stroke="#fff"
+            stroke-width="2"
+          />
+          <line
+            id="Line_94"
+            data-name="Line 94"
+            y1="36.163"
+            x2="34.163"
+            transform="translate(1642.5 5599.663)"
+            fill="none"
+            stroke="#fff"
+            stroke-width="2"
+          />
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+function SamplePrevArrow(props) {
+  const { onClick } = props
+  return (
+    <div
+      className="slideshow-prev"
+      onClick={onClick}
+      onKeyDown={onClick}
+      role="button"
+      tabIndex={0}
+      aria-label="go to previous"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.617 72.699">
+        <g
+          id="Group_62"
+          data-name="Group 62"
+          transform="translate(0.727 0.687)"
+        >
+          <line
+            id="Line_93"
+            data-name="Line 93"
+            x1="34.163"
+            y2="36.163"
+            transform="translate(0)"
+            fill="none"
+            stroke="#fff"
+            stroke-width="2"
+          />
+          <line
+            id="Line_94"
+            data-name="Line 94"
+            x1="34.163"
+            y1="36.163"
+            transform="translate(0 35.163)"
+            fill="none"
+            stroke="#fff"
+            stroke-width="2"
+          />
+        </g>
+      </svg>
+    </div>
+  )
+}
+
 const TextImage = ({ data, picPosition }) => {
   const { title, tagline, text, slideshow, linkedPdf, linkText } = data
-  const settings = {
+  const mobileSettings = {
     infinite: true,
     fade: true,
     speed: 500,
     arrows: false,
     swipeToSlide: true,
-    pauseOnHover: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
   }
+
+  const settings = {
+    infinite: true,
+    fade: true,
+    speed: 500,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
   return (
     <div className="section-container">
       <section id={title} className="section">
@@ -37,7 +133,7 @@ const TextImage = ({ data, picPosition }) => {
             <Fade>
               <h2 className="section-title">{title}</h2>
             </Fade>
-            <Slider {...settings} className="mobile-section-pic">
+            <Slider {...mobileSettings} className="mobile-section-pic">
               {slideshow.map(pic => (
                 <GatsbyImage
                   key={pic.id}
